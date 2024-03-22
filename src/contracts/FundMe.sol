d867722de49e7dbb1a54730e8d12e3dc95ec122e
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-03-23 01:28:36 +0600

    withdraw and fund feature

--- src/contracts/FundMe.sol
+++ src/contracts/FundMe.sol
@@ -1,15 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe{
-    uint256 public minimumUsd = 50 * 1e18;
+    using PriceConverter for uint;
 
-    function fund() public payable {
-        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send Enough");  // 1e18 == 1 * 10 ** 18
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+    address public owner;
+    address[] public funders;
+    mapping (address => uint) public addressToAmountFunded;
+
+    constructor(){
+         owner = msg.sender;
     }
 
-    function getPrice() public view returns(uint) {
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send Enough");  // 1e18 == 1 * 10 ** 18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+      function getPrice() public  view returns(uint) {
         // ABI
         // Address : 0x694AA1769357215DE4FAC081bf1f309aDC325306
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
@@ -17,14 +29,32 @@ contract FundMe{
         return uint(price * 1e10);
     }
 
-    function getVersion() public view  returns (uint) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
+    function withdraw() public onlyOwner {
+        for(uint funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        // reset the array
+        funders = new address[](0);
+
+        // actually withdraw fund in three ways
+        // transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        bool isSuccess = payable(msg.sender).send(address(this).balance);
+        require(isSuccess, "Send Failed!");
+
+        // call
+        // without ABI Calling
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, 'Call Failed!');
     }
-    
-    function getConversionRate(uint ethAmount) public view returns (uint) {
-        uint ethPrice = getPrice();
-        uint ethAmountInUsd = (ethPrice * ethAmount) / (1e18);
-        return (ethAmountInUsd);
+
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
     }
 }
