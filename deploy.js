f533c5e6f2aafdbbe0f026b595fb10a628c59957
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-03-24 09:43:36 +0600

    transacted in my first local-blockchain setup

--- deploy.js
+++ deploy.js
@@ -1,5 +1,15 @@
-async function helllo() {
-    console.log('Hi');
+import { ContractFactory, JsonRpcProvider, Wallet } from "ethers";
+import { readFileSync } from "fs";
+
+async function main() {
+    let provider = new JsonRpcProvider("HTTP://127.0.0.1:7545");
+    let wallet = new Wallet("0x7d93bdb32deaa87de3c360ee327f887ce455c3a9579fe432b87236483d656902", provider);
+    const abi = readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+    const binary = readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
+    const contractFactory = new ContractFactory(abi, binary, wallet);
+    console.log("Deploying, please wait...");
+    const contract = await contractFactory.deploy();
+    console.log(contract);
 }
 
-helllo();
+main();
